42f494d11b48afea4f58ba79e3d345fb8cb131b5
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-06-13 16:58:53 -0300

    Cadastrando post

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,6 @@ export default createAppContainer(
       New
     },
     {
-      initialRouteName: 'New',
       defaultNavigationOptions: {
         headerTintColor: '#000',
         headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
